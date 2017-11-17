c4aeb86f3d94862e59d1a7fed5665f42c59496ef
angela <33108490+z0m8i3@users.noreply.github.com>
2017-11-17 12:50:30 -0600

    update-check.sh
    update checker created

---
+++ update-check.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+#check for updates
+UPDATES_COUNT=$(yum check-update --quiet | grep -v "^$" | wc -l)
+UPDATES=$(yum list updates)
+
+if [[ $UPDATES_COUNT -gt 0 ]]; then
+  #we have updates; record to motd
+  echo "echo \"${UPDATES_COUNT} Updates available\"" > /etc/update-motd.d/90-updates-available
+  #and send root a message
+  echo $UPDATES | mail -s "${UPDATES_COUNT} Updates available for $(hostname)" root
+else
+  echo "echo \"System is up to date\"" > /etc/update-motd.d/90-updates-available
+fi
