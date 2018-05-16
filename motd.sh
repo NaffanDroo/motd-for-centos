727c4365120c8f0d97b02e2ccd05d424d1036707
angela <33108490+angela-d@users.noreply.github.com>
2018-05-15 19:14:46 -0500

    legacy code update
    Signed-off-by: angela <33108490+angela-d@users.noreply.github.com>

--- motd.sh
+++ motd.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
-#
+#!/bin/bash
+
 DISTRIB_DESCRIPTION=$(cat /etc/centos-release)
 
 #change 'CentOS' to something else, if you want to name your server or display a phrase
 DISPLAYNAME=CentOS
-LOLCAT=/usr/local/bin/lolcat
+LOLCAT=/usr/local/bin/lolcat #whereis lolcat if your system put it somewhere else
 
 figlet $DISPLAYNAME -c | $LOLCAT -f
 printf "\n"
@@ -12,47 +12,39 @@ printf "\n"
 printf "\t%s \tKernel: %s\n" "$DISTRIB_DESCRIPTION" "$(uname -r)"
 printf "\n"
 
-#System date
-date=`date`
 
-#System load
-LOAD1=`cat /proc/loadavg | awk {'print $1'}`
-LOAD5=`cat /proc/loadavg | awk {'print $2'}`
-LOAD15=`cat /proc/loadavg | awk {'print $3'}`
+date=$(date)
+LOAD1=$(cat /proc/loadavg | awk '{print $1}')
+LOAD5=$(cat /proc/loadavg | awk '{print $2}')
+LOAD15=$(cat /proc/loadavg | awk '{print $3}')
 
-#System uptime
-uptime=`cat /proc/uptime | cut -f1 -d.`
+# uptime
+uptime=$(cat /proc/uptime | cut -f1 -d.)
 upDays=$((uptime/60/60/24))
 upHours=$((uptime/60/60%24))
 upMins=$((uptime/60%60))
 upSecs=$((uptime%60))
 
-#Root fs info
-root_usage=`df -h / | awk '/\// {print $4}'|grep -v "^$"`
+# filesystem info
+root_usage=$(df -h / | awk '/\// {print $4}'|grep -v "^$")
 
-#Memory Usage
-memory_usage=`free -m | awk '/Mem:/ { total=$2 } /buffers\/cache/ { used=$3 } END { printf("%3.1f%%", used/total*100)}'`
-swap_usage=`free -m | awk '/Swap/ { printf("%3.1f%%", $3/$2*100) }'`
+# memory
+memory_usage=$(free -t -g | grep Total | awk '{print $3;}')
+total_memory=$(free -t -g | grep "Mem" | awk '{print $2" GB";}')
+swap_usage=$(free -m | awk '/Swap/ { printf("%3.1f%%", $3/$2*100) }')
 
-#Users
-users=`users | wc -w`
+# users
+users=$(users | wc -w)
 
-#Processes
-processes=`ps aux | wc -l`
+# processes
+processes=$(ps aux | wc -l)
 
-printf "\tSystem information as of: $date"
-
-echo
-echo
-
-printf "\tSystem Load:\t%s %s %s\tSystem Uptime:\t\t%s "days" %s "hours" %s "min" %s "sec"\n" $LOAD1, $LOAD5, $LOAD15 $upDays $upHours $upMins $upSecs
-printf "\tMemory Usage:\t%s\t\t\tSwap Usage:\t\t%s\n" $memory_usage $swap_usage
-printf "\tUsage:\t\t%s\t\t\tSFTP Accounts:\t\t%s\n" $root_usage $users
-printf "\tProcesses:\t%s\t\t\t\n" $processes
-
-echo
-echo
+printf "\tSystem information as of: %s\n\n" "$date"
+printf "\tSystem Load:\t%s %s %s\tSystem Uptime:\t\t%s days %s hours %s min %s sec\n" "$LOAD1", "$LOAD5", "$LOAD15" "$upDays" "$upHours" "$upMins" "$upSecs"
+printf "\tMemory Usage:\t%s\t\t\tSwap Usage:\t\t%s\n" "$memory_usage"/"$total_memory" "$swap_usage"
+printf "\tUsage:\t\t%s\t\t\tSFTP Accounts:\t\t%s\n" "$root_usage" "$users"
+printf "\tProcesses:\t%s\t\t\t\n\n" "$processes"
 
 #include the updater; sourced from update-check.sh
 source /etc/update-motd.d/90-updates-available
-echo
+printf "\n\n"
